203019e5917175a7253d3b4c2a4663621e024512
Code Sandbox <hello@codesandbox.io>
2021-10-31 13:37:50 +0000

    Deployment from Codesandbox.

--- game.js
+++ game.js
@@ -35,7 +35,7 @@ function Bear() {
 }
 function start() {
   //create bear
-  bear = new Bear();
+  this.bear = new Bear();
   // Add an event listener to the keypress event.
   document.addEventListener("keydown", moveBear, false);
 }
@@ -48,15 +48,15 @@ function moveBear(e) {
   const KEYLEFT = 37;
   const KEYRIGHT = 39;
   if (e.keyCode == KEYRIGHT) {
-    bear.move(1, 0);
+    this.bear.move(1, 0);
   } // right key
   if (e.keyCode == KEYLEFT) {
-    bear.move(-1, 0);
+    this.bear.move(-1, 0);
   } // left key
   if (e.keyCode == KEYUP) {
-    bear.move(0, -1);
+    this.bear.move(0, -1);
   } // up key
   if (e.keyCode == KEYDOWN) {
-    bear.move(0, 1);
+    this.bear.move(0, 1);
   } // down key
 }
